83199e4c8ce6f3876e20307ef08e305bba34dcfc
Nolan Kuenzi <mr.nolank@gmail.com>
2019-06-08 19:36:54 -0700

    babel & p_jsn updates

--- public/index.js
+++ public/index.js
@@ -6,43 +6,35 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
   });
   var width = 900;
   var height = 600;
-
   var svg = d3.select("svg").attr("id", "svg").attr("width", width).attr("height", height);
-
-  var margin = { top: 85, right: 100, bottom: 90, left: 90 };
+  var margin = {
+    top: 85,
+    right: 100,
+    bottom: 90,
+    left: 90
+  };
   var viewWidth = +svg.attr("width") - margin.left - margin.right;
   var viewHeight = +svg.attr("height") - margin.top - margin.bottom;
-
   var xAxisScale = d3.scaleLinear().domain([d3.min(data, function (d) {
     return d[0].slice(0, 4);
   }), d3.max(data, function (d) {
     return d[0].slice(0, 4);
   })]).range([0, viewWidth]);
-
   var yAxisScale = d3.scaleLinear().domain([d3.min(data, function (d) {
     return d[1];
   }), d3.max(data, function (d) {
     return d[1];
   })]).range([viewHeight, 0]);
-
   var xAxisCall = d3.axisBottom(xAxisScale).tickFormat(d3.format("d"));
-
   var yAxisCall = d3.axisLeft(yAxisScale);
-
   var G = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
   G.append("g").attr("id", "x-axis").attr("transform", "translate(" + 0 + "," + viewHeight + ")").call(xAxisCall);
-
   G.append("g").attr("id", "y-axis").style("color", "purple").call(yAxisCall);
-
   svg.append("text").attr("class", "y-label").attr("text-anchor", "end").attr("y", 110).attr("x", -120).attr("transform", "rotate(-90)").text("Gross Domestic Product");
-
   var ticks = d3.selectAll("tick, text");
   ticks.attr("class", "tick");
   ticks.style("fill", "purple");
-
   var toolTip = d3.select("body").append("div").attr("id", "toolTip");
-
   var bars = d3.select("svg").append("g").selectAll("rect").data(data).enter().append("rect").attr("class", "bar").attr("data-date", function (d) {
     return d[0];
   }).attr("data-gdp", function (d) {
@@ -54,7 +46,8 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
   }).attr("height", function (d, i) {
     return 430 - yAxisScale(d[1]);
   }).attr("width", viewWidth / data.length).style("fill", "purple").on("mouseover", function (d) {
-    var quater = void 0;
+    var quater;
+
     if (d[0][6] === "1") {
       quater = "Q1";
     } else if (d[0][6] === "4") {
@@ -64,28 +57,31 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
     } else {
       quater = "Q4";
     }
+
     var format = d3.format(",");
     toolTip.attr("data-date", d[0]).style("left", d3.event.pageX + 3 + "px").style("top", d3.event.pageY + "px").style("display", "inline-block").html(function () {
-      return d[0].slice(0, 4) + " quater:<br>$" + format(d[1]) + " Billion";
+      return "".concat(d[0].slice(0, 4), " quater:<br>$").concat(format(d[1]), " Billion");
     });
     d3.select(this).style("fill", "white");
   }).on("mouseleave", function (d) {
     toolTip.style("display", "none");
     d3.select(this).style("fill", "purple");
   });
-}).catch(function (error) {
+})["catch"](function (error) {
   alert("Data failed to load, please try again.");
 });
-
 /* For Mobile Devices */
+
 var clear = document.getElementById("body");
 clear.addEventListener("touchstart", function (e) {
   if (e.target.className.baseVal !== "bar") {
     var barColor = document.getElementsByClassName("bar");
     var toolT = document.getElementById("toolTip");
+
     for (var i = 0; i < barColor.length; i++) {
       barColor[i].style.fill = "purple";
     }
+
     toolT.style.display = "none";
   }
 });
